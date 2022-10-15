d2babc346520774aba2d2c64b037319e207f6dc7
IfSince <leonlaade@gmx.de>
2022-10-15 18:43:10 +0200

    use divs instead of svg for planets

--- scripts/shared/main.js
+++ scripts/shared/main.js
@@ -1,13 +1,15 @@
 import {createMenuEventListener} from "./create-menu-event-listener.js";
 import {updateBackground} from "./update-background.js";
 
-const planetElement = document.querySelector('.planets');
+const planetElements = document.querySelectorAll('.planet');
 const starElement = document.querySelector('.stars');
 const menuBtn = document.querySelector("#menu-icon");
 
 createMenuEventListener(menuBtn);
 
 document.body.addEventListener("mousemove", (e) => {
-    updateBackground(planetElement, e.pageX, e.pageY, -0.02);
+    [...planetElements].forEach((element) => {
+        updateBackground(element, e.pageX, e.pageY, -0.01);
+    })
     updateBackground(starElement, e.pageX, e.pageY, 0.005);
 });
