46e293f50ef2b48d5e7a9ba4c1b015c16f1382f2
IfSince <leonlaade@gmx.de>
2022-10-16 18:16:47 +0200

    fix navigation transition from desktop to mobile

--- scripts/shared/create-menu-event-listener.js
+++ scripts/shared/create-menu-event-listener.js
@@ -1,4 +1,22 @@
+/**
+ * Always adds menu-transition class when button is clicked and remove it after.
+ * This fixes the problem with the transition also taking place when switching between desktop and mobile menu.
+ * Manually remove transition-delay with timeOut so that it only delays on closing.
+ * @param element {Element}
+ */
+
 export function createMenuEventListener(element) {
     const navigation = document.querySelector("nav");
-    element.addEventListener("click", () => navigation.classList.toggle("menu-open"));
+    element.addEventListener("click", () => {
+        if (navigation.classList.contains("menu-transition")) {
+            navigation.style.transitionDelay = "0.35s";
+            setTimeout(() => {
+                navigation.style.transitionDelay = "0s";
+                navigation.classList.remove("menu-transition");
+            }, 200);
+        } else {
+            navigation.classList.add("menu-transition");
+        }
+        navigation.classList.toggle("menu-open");
+    });
 }
